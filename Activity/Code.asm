65c98b562d64c4d4a7ebc9a62bc0d26f686d80f5
L10NH34RT <zleo050907@gmail.com>
2025-06-22 22:53:12 -0700

    Update and rename Code.s to Code.asm

--- Activity/Code.asm
+++ Activity/Code.asm
@@ -2,15 +2,15 @@
   msg: .ascii "I came,\nI saw,\nI conquered.\n"
 
 .section .text
-  .global _start
+.global _start
 
 _start:
-  move $1, %rax
+  mov $1, %rax
   mov $1, %rdi
   mov $msg, %rsi
   mov $msg_len, %rdx
   syscall
 
-  mov $60, %rav
-  mov $0, %rav
+  mov $60, %rax
+  mov $0, %rdi
   syscall
